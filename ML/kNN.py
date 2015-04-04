2d4db9c220de815d1387a821bfd41d244dc99abf
hnldxwj <hnldxwj123@gmail.com>
2015-04-04 21:57:12 +0800

    2015.4.4 kNN

---
+++ ML/kNN.py
@@ -0,0 +1,17 @@
+'''
+Created on 2015Äê4ÔÂ4ÈÕ
+
+@author: Administrator
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
