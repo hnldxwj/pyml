a88e43bc2e144b11febc69df931a708d852e172c
hnldxwj <hnldxwj123@gmail.com>
2015-04-06 19:36:07 +0800

    kNN.py

--- ML/kNN.py
+++ ML/kNN.py
@@ -9,42 +9,42 @@ from DataStructure import BinarySearchArray
 
 class kNN:
 
-       @staticmethod
-       def calc(trainfile,testfile,outputfile,k):
-           if k==0:
-               return False
-           trainset=FileImport.file2matrix(trainfile)
-           trainset=FileImport.normalization(trainset)
-           testset=FileImport.file2matrix(testfile)
-           testset=FileImport.normalization(testset)
-           fw=open(outputfile,'w')
-           for row in testset:
-               result=kNN.calcOneCase(trainset,row,k)
-               #print "result:"+str(result)
-               fw.write(str(result)+"\n")
-           fw.close()
-           return True
+        @staticmethod
+        def calc(trainfile,testfile,outputfile,k):
+            if k==0:
+                return False
+            trainset=FileImport.file2matrix(trainfile)
+            trainset=FileImport.normalization(trainset)
+            testset=FileImport.file2matrix(testfile)
+            testset=FileImport.normalization(testset)
+            fw=open(outputfile,'w')
+            for row in testset:
+                result=kNN.calcOneCase(trainset,row,k)
+                #print "result:"+str(result)
+                fw.write(str(result)+"\n")
+            fw.close()
+            return True
+  
 
+        @staticmethod
+        def calcOneCase(trainset,testrow,k):
 
-       @staticmethod
-       def calcOneCase(trainset,testrow,k):
+            bsa=BinarySearchArray(k)
+            bsa.setType(1)
+            for trainrow in trainset:
+                distance=kNN.calcDistance(trainrow,testrow)
+                node=Node(distance,trainrow[0])
+                bsa.insert(node)
 
-           bsa=BinarySearchArray(k)
-           bsa.setType(1)
-           for trainrow in trainset:
-               distance=kNN.calcDistance(trainrow,testrow)
-               node=Node(distance,trainrow[0])
-               bsa.insert(node)
+            return bsa.findMost()
 
-           return bsa.findMost()
-
-       @staticmethod
-       def calcDistance(trainrow,testrow):
-           val=0
-           for x in range(1,min(len(trainrow),len(testrow))):
-               val+=((trainrow[x]-testrow[x])**2)
-           val=val**0.5
-           return val
+        @staticmethod
+        def calcDistance(trainrow,testrow):
+            val=0
+            for x in range(1,min(len(trainrow),len(testrow))):
+                val+=((trainrow[x]-testrow[x])**2)
+            val=val**0.5
+            return val
 
 
 
