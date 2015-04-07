8dcb4a30fb8d82c37fd210bedbf3c6ee4a9136fc
hnldxwj <hnldxwj123@gmail.com>
2015-04-07 23:16:20 +0800

    2015.4.7

---
+++ ML/NaiveBayesian.py
@@ -0,0 +1,26 @@
+class NaiveBayesian:
+
+    @staticmethod
+    def loadDataSet:
+
+        return
+
+    @staticmethod
+    def createWordList(dataSet):
+        WordSet=set([])
+        for data in dataSet:
+            WordSet=WordSet | set(data) #combination
+        return list(WordSet)
+
+
+
+    @staticmethod
+    def findWordInInput(dataSet,InputSet):
+        returnArray=[0]*dataSet
+        for word in InputSet:
+            if word in dataSet:
+                returnArray[dataSet.index(word)]=1
+        return returnArray
+
+    @staticmethod
+    def
